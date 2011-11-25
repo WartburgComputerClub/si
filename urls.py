34c6483137e5a830a7fc491c060aabaef52f2c71
Andrew Reisner <andrew.reisner@gmail.com>
2011-11-25 06:49:24 -0600

    finished password reset

--- urls.py
+++ urls.py
@@ -2,13 +2,13 @@ from django.conf.urls.defaults import patterns,include,url
 from si.siadmin import site
 from django.contrib.auth import views as auth_views
 urlpatterns = patterns('si.views',
-    (r'^$','index'),                       
+    url(r'^$','index',name='index'),                       
     (r'^signin/(?P<session>\d+)/$','signin'),
     (r'^admin/',include(site.urls)),
     (r'^register/$','register'),
     (r'^leave/$','leave'),                       
-url(r'^passreset/$',auth_views.password_reset,name='forgot_password1'),
-url(r'^passresetdone/$',auth_views.password_reset_done,name='forgot_password2'),
-url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,name='forgot_password3'),
-url(r'^passresetcomplete/$',auth_views.password_reset_complete,name='forgot_password4'),
+url(r'^passreset/$',auth_views.password_reset,{'template_name': 'si/password_reset_form.html'},name='forgot_password1'),
+url(r'^passresetdone/$',auth_views.password_reset_done,{'template_name':'si/password_reset_done.html'},name='forgot_password2'),
+url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,{'template_name':'si/password_reset_confirm.html'},name='forgot_password3'),
+url(r'^passresetcomplete/$',auth_views.password_reset_complete,{'template_name':'si/password_reset_complete.html'},name='forgot_password4'),
 )
