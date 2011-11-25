df13f545dc8b7bcc508f5daeb693f55da45b589f
Andrew Reisner <andrew@andrewreisner.com>
2011-11-25 13:14:21 -0600

    added registration templates

--- urls.py
+++ urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls.defaults import patterns,include,url
 from si.siadmin import site
-
+from django.contrib.auth import views as auth_views
 urlpatterns = patterns('si.views',
     (r'^$','index'),                       
     (r'^signin/(?P<session>\d+)/$','signin'),
     (r'^admin/',include(site.urls)),
     (r'^register/$','register'),
     (r'^leave/$','leave'),                       
+url(r'^passreset/$',auth_views.password_reset,name='forgot_password1'),
+url(r'^passresetdone/$',auth_views.password_reset_done,name='forgot_password2'),
+url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,name='forgot_password3'),
+url(r'^passresetcomplete/$',auth_views.password_reset_complete,name='forgot_password4'),
 )
