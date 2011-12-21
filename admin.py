046e6f2ee5ca29ebe771635cd33ba620abb6bd37
Andrew Reisner <andrew@andrewreisner.com>
2011-12-20 18:28:30 -0600

    added favicon

--- admin.py
+++ admin.py
@@ -2,6 +2,6 @@ from si.models import Student,Course,Session
 from django.contrib import admin
 
 
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Session)
+#admin.site.register(Student)
+#admin.site.register(Course)
+#admin.site.register(Session)
